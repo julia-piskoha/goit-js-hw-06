100fda55facdb31058b5a23edec65187b3d242d9
julia-piskoha <upiskoha@gmail.com>
2022-11-09 22:19:36 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listEl = document.querySelector("#ingredients");
+const makeIngredientsList = (ingredient) => {
+  return ingredients.map((ingredient) => {
+    const itemEl = document.createElement("li");
+    itemEl.textContent = ingredient;
+    itemEl.classList.add("item");
+
+    return itemEl;
+  });
+};
+const elements = makeIngredientsList(ingredients);
+console.log(elements);
+listEl.append(...elements);
